4f2e05d50e2d1a06b0e81307d688410ba8078aa8
Dano Oravec <danoravec@gmail.com>
2017-08-17 18:58:22 +0200

    Program searches for accessible browser

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def print_result(route):
 
 if __name__ == '__main__':
     print('Informácie o linke')
-    route_source = 'Košice, žel. st.'  # input('Linka začína v stanici: ')
-    route_target = 'Praha, hl.n.'  # input('Linka končí v stanici: ')
-    source = 'Ružomberok, žel. st.'  # input('Ty ideš zo stanice: ')
-    target = 'Praha, hl.n.'  # input('Do stanice: ')
-    date = '17/08/2017'  # input('Dátum (dd/mm/rrrr): ')
-    departure_time = '23:45'  # input('Čas (hh:mm): ')
+    route_source = input('Linka začína v stanici: ')
+    route_target = input('Linka končí v stanici: ')
+    source = input('Ty ideš zo stanice: ')
+    target = input('Do stanice: ')
+    date = input('Dátum (dd/mm/rrrr): ')
+    departure_time = input('Čas (hh:mm): ')
     print('\n------- Idem cestovať... -------\n')
     finder = PathFinder(route_source + ' ' + route_target, source, target, date, departure_time)
     found_route = finder.find_path()
